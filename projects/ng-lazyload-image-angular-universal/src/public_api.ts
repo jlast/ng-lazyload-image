61898b5eef99a09b2eea1cc0493b0f98433963fc
Jeroen van der Last <jeroen.van.der.last@essent.nl>
2019-01-23 11:54:59 +0100

    Fork it

---
+++ projects/ng-lazyload-image-angular-universal/src/public_api.ts
@@ -0,0 +1,7 @@
+import { LazyLoadImageDirective } from './lib/lazyload-image.directive';
+import { LazyLoadImageModule } from './lib/lazyload-image.module';
+import { intersectionObserverPreset } from './lib/intersection-observer-preset';
+import { scrollPreset } from './lib/scroll-preset';
+
+export { LazyLoadImageDirective, LazyLoadImageModule, intersectionObserverPreset, scrollPreset };
+export default LazyLoadImageModule;
